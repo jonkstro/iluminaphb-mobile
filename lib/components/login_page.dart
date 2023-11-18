8e85bbd60cb1e58a644288186472bb1afc2becd8
jonas castro <jonascastro.dev@gmail.com>
2023-11-18 20:16:14 -0300

    Construção da tela de login

---
+++ lib/components/login_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/app_bg.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+        ),
+        body: Center(
+          child: Text('Olá mundo tcc'),
+        ),
+      ),
+    );
+  }
+}
